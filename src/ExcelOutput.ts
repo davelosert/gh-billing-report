2381a2a14f0e639a01e81b11900edbcb97880f35
David Losert <davelosert@github.com>
2024-01-19 12:56:01 +0100

    feat: Adjusts formatting of excel file

--- src/ExcelOutput.ts
+++ src/ExcelOutput.ts
@@ -7,25 +7,38 @@ const generateExcel = async (outputFile: string, usageItems: UsageItem[]) => {
 	const orgWorksheet = workbook.addWorksheet('Usage by Organization');
 	
 	const organizationReport = createOrganizationReport(usageItems);
-	
+
 	orgWorksheet.columns = [	
 			{ header: 'Organization', key: 'organization', width: 30 },
 			{ header: 'Gross Amount', key: 'grossAmount', width: 20 },
-			{ header: 'Discount Amount', key: 'discountAmount', width: 20},
-			{ header: 'Net Amount (actually billed)', key: 'netAmount', width: 30 },
+			{ header: 'Applied Discount', key: 'discountAmount', width: 22 },
+			{ header: 'Net Amount (actually billed)', key: 'netAmount', width: 40 },
 	]
-	
-	orgWorksheet.getRow(1).font = { bold: true, underline: true };
 
-	orgWorksheet.getColumn('netAmount').numFmt = '$#,##0.00;[Red]-$#,##0.00';
-	orgWorksheet.getColumn('grossAmount').numFmt = '$#,##0.00;[Red]-$#,##0.00';
+
+	const grossCol = orgWorksheet.getColumn('grossAmount');
+	grossCol.numFmt = '$#,##0.00;[Red]-$#,##0.00';
+	grossCol.font = { color: { argb: '808080' } };
+
+	const discountCol = orgWorksheet.getColumn('discountAmount');
+	discountCol.numFmt = '$#,##0.00;[Red]-$#,##0.00';
+	discountCol.font = { color: { argb: '808080' } };
+
+	const netCol = orgWorksheet.getColumn('netAmount')
+	netCol.numFmt = '$#,##0.00;[Red]-$#,##0.00';
 
 	orgWorksheet.addRows(organizationReport.usageByOrg)
-	
-	const sumRow = orgWorksheet.addRow(['Total', organizationReport.sumUsage.netAmount, organizationReport.sumUsage.grossAmount]);
+
+	const sumRow = orgWorksheet.addRow(['Total', organizationReport.sumUsage.grossAmount, organizationReport.sumUsage.discountAmount, organizationReport.sumUsage.netAmount]);
 	sumRow.font = { bold: true };
 
-	console.log(`Overall Usage: ${organizationReport.sumUsage.netAmount} (net) ${organizationReport.sumUsage.grossAmount} (gross)`);
+	// Overwrite fonts for first row AFTER applying column styling
+	const firstRow = orgWorksheet.getRow(1);
+	firstRow.font = { bold: true, underline: true, size: 16 };
+	firstRow.getCell(1).alignment = { horizontal: 'left' };
+	firstRow.getCell(2).alignment = { horizontal: 'right' };
+	firstRow.getCell(3).alignment = { horizontal: 'right' };
+	firstRow.getCell(4).alignment = { horizontal: 'right' };
 
 	const detailUsage = workbook.addWorksheet('Detail Usage');
 	// write all the entries of the usageItems array to the excel file
